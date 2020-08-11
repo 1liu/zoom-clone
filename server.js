90aeeb2b994cbbf87a637663dacc9d4bfd344a91
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-08-11 10:59:02 -0700

    generated unique userId with PeerJS

--- server.js
+++ server.js
@@ -2,17 +2,28 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const {v4: uuidV4} = require('uuid');
+const { v4: uuidV4 } = require('uuid');
+require('dotenv').config();
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
   res.redirect(`${uuidV4()}`);
 });
 
-app.get('/:room', (req, res)=>{
-  res.render('room', {roomId: req.params.room})
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
 });
 
-server.listen(3000)
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+  })
+})
+
+const port = process.env.SERVER_PORT || 5000;
+server.listen(port, () => {
+  console.log('Server is listening at port ', port)
+})
